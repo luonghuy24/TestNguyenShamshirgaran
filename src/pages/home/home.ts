b58577d116cb27335edf70dd7349862a79aabd8d
luonghuy24 <mrluonghuy@gmail.com>
2018-03-15 18:50:34 -0400

    todo app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
+import { AddItemPage } from '../add-item/add-item';
+import { ItemDetailPage } from '../item-detail/item-detail';
+import { DataProvider } from '../../providers/data/data';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,55 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public items = [];
 
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public dataService: DataProvider) {
+    this.dataService.getData().then((todos) => {
+
+      if(todos){
+        this.items = todos;
+      }
+
+    });
+  }
+
+  ionViewDidLoad(){
+
+    // this.items = [
+    //   {title: 'hi1', description: 'test1'},
+    //   {title: 'hi2', description: 'test2'},
+    //   {title: 'hi3', description: 'test3'}
+    // ];
+
+  }
+
+  addItem(){
+    let addModal = this.modalCtrl.create(AddItemPage);
+
+    addModal.onDidDismiss((item) => {
+
+          if(item){
+            this.saveItem(item);
+          }
+
+    });
+
+    addModal.present();
+  }
+
+  saveItem(item){
+    this.items.push(item);
+    this.dataService.save(this.items);
+  }
+
+  removeItem(item){
+
+  }
+
+  viewItem(item){
+    this.navCtrl.push(ItemDetailPage, {
+      item: item
+    });
   }
 
 }
---
+++ src/providers/data/data.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the DataProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class DataProvider {
+
+  // constructor(public http: HttpClient) {
+  //   console.log('Hello DataProvider Provider');
+  // }
+
+  constructor(public storage: Storage){
+
+  }
+
+  getData() {
+    return this.storage.get('todos');
+  }
+
+  save(data){
+    this.storage.set('todos', data);
+  }
+
+}
